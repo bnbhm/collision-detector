cf65f781f4128e7775613433ed9fb039e02779fa
Vishal <95016059+haxfn@users.noreply.github.com>
2026-02-16 23:01:20 +0530

    add collision info: vertice

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@ use std::f32::consts::TAU;
 
 #[macroquad::main("Braideroids : Asteroids = Braid;")]
 async fn main() {
-    let mut asteroid1 = Asteroid::new(Body::new(100.0, 100.0, 20.0, 50.0, 0.0, 2.0), 3, 120.0);
-    let mut asteroid2 = Asteroid::new(Body::new(250.0, 100.0, 100.0, -20.0, 5.0, -0.4), 6, 50.0);
+    let mut asteroid1 = Asteroid::new(Body::new(250.0, 250.0, -20.0, 10.0, -2.0, 0.5), 3, 120.0);
+    let mut asteroid2 = Asteroid::new(Body::new(250.0, 250.0, 30.0, -30.0, 4.0, -1.0), 3, 50.0);
+    let mut asteroid3 = Asteroid::new(Body::new(250.0, 250.0, 40.0, -50.0, 5.0, -0.3), 3, 50.0);
+    let mut asteroid4 = Asteroid::new(Body::new(250.0, 250.0, -50.0, -10.0, 3.0, 0.5), 5, 100.0);
 
     let mut game_last_tick = get_time() as f32;
     loop {
@@ -14,44 +16,82 @@ async fn main() {
 
         asteroid1.body.update(dt);
         asteroid2.body.update(dt);
+        asteroid3.body.update(dt);
+        asteroid4.body.update(dt);
 
         reflect(&mut asteroid1.body);
         reflect(&mut asteroid2.body);
-
-        let collided = collision(&asteroid1, &asteroid2);
+        reflect(&mut asteroid3.body);
+        reflect(&mut asteroid4.body);
 
         asteroid1.draw();
         asteroid2.draw();
-        if collided {
-            draw_poly(
-                asteroid1.body.lin_pos.x,
-                asteroid1.body.lin_pos.y,
-                asteroid1.sides,
-                asteroid1.size,
-                asteroid1.body.ang_pos.to_degrees(),
-                WHITE,
-            );
-            draw_poly(
-                asteroid2.body.lin_pos.x,
-                asteroid2.body.lin_pos.y,
-                asteroid2.sides,
-                asteroid2.size,
-                asteroid2.body.ang_pos.to_degrees(),
-                WHITE,
-            );
-        }
+        asteroid3.draw();
+        asteroid4.draw();
 
-        let vertices1 = asteroid1.shape();
-        vertices1.iter().for_each(|vertice| {
-            draw_circle(vertice.x, vertice.y, 5.0, BLUE);
-        });
+        highlight_collision(&asteroid1, &asteroid2);
+        highlight_collision(&asteroid1, &asteroid3);
+        highlight_collision(&asteroid1, &asteroid4);
 
-        let vertices2 = asteroid2.shape();
-        vertices2.iter().for_each(|vertice| {
-            draw_circle(vertice.x, vertice.y, 5.0, BLUE);
-        });
+        highlight_collision(&asteroid2, &asteroid3);
+        highlight_collision(&asteroid2, &asteroid4);
+
+        highlight_collision(&asteroid3, &asteroid4);
 
         next_frame().await;
+    }
+}
+
+fn draw_vertices(object: &impl Shape) {
+    let vertices1 = object.shape();
+    vertices1.iter().for_each(|vertice| {
+        draw_circle(vertice.x, vertice.y, 5.0, BLUE);
+    });
+}
+
+fn highlight_collision(asteroid1: &Asteroid, asteroid2: &Asteroid) {
+    let collided = collision(asteroid1, asteroid2);
+    if let Some(collision_info) = collided {
+        /* draw_poly(
+            asteroid1.body.lin_pos.x,
+            asteroid1.body.lin_pos.y,
+            asteroid1.sides,
+            asteroid1.size,
+            asteroid1.body.ang_pos.to_degrees(),
+            GRAY,
+        );
+        draw_poly(
+            asteroid2.body.lin_pos.x,
+            asteroid2.body.lin_pos.y,
+            asteroid2.sides,
+            asteroid2.size,
+            asteroid2.body.ang_pos.to_degrees(),
+            GRAY,
+        ); */
+        collision_info.which_object_which_vertice[0]
+            .iter()
+            .for_each(|vertice_id| {
+                let vertice = asteroid1.shape()[*vertice_id];
+                draw_circle(vertice.x, vertice.y, 4.0, RED);
+
+                let vertice_p = asteroid1.shape()
+                    [(*vertice_id + asteroid1.shape().len() - 1) % asteroid1.shape().len()];
+                let vertice_n = asteroid1.shape()[(*vertice_id + 1) % asteroid1.shape().len()];
+                draw_line(vertice.x, vertice.y, vertice_p.x, vertice_p.y, 3.0, RED);
+                draw_line(vertice.x, vertice.y, vertice_n.x, vertice_n.y, 3.0, RED);
+            });
+        collision_info.which_object_which_vertice[1]
+            .iter()
+            .for_each(|vertice_id| {
+                let vertice = asteroid2.shape()[*vertice_id];
+                draw_circle(vertice.x, vertice.y, 4.0, RED);
+
+                let vertice_p = asteroid2.shape()
+                    [(*vertice_id + asteroid2.shape().len() - 1) % asteroid2.shape().len()];
+                let vertice_n = asteroid2.shape()[(*vertice_id + 1) % asteroid2.shape().len()];
+                draw_line(vertice.x, vertice.y, vertice_p.x, vertice_p.y, 3.0, RED);
+                draw_line(vertice.x, vertice.y, vertice_n.x, vertice_n.y, 3.0, RED);
+            });
     }
 }
 
@@ -81,18 +121,36 @@ impl Shape for Asteroid {
     }
 }
 
-fn collision(object1: &impl Shape, object2: &impl Shape) -> bool {
-    for vertice in object1.shape() {
+struct CollisionInfo {
+    which_object_which_vertice: [Vec<usize>; 2],
+}
+
+fn collision(object1: &impl Shape, object2: &impl Shape) -> Option<CollisionInfo> {
+    let mut collided = false;
+
+    let mut collision_info = CollisionInfo {
+        which_object_which_vertice: [Vec::new(), Vec::new()],
+    };
+
+    for (it, vertice) in object1.shape().iter().enumerate() {
         if inside(&vertice, object2) {
-            return true;
+            collision_info.which_object_which_vertice[0].push(it);
+            collided = true;
         }
     }
-    for vertice in object2.shape() {
-        if inside(&vertice, object1) {
-            return true;
+
+    for (it, vertice) in object2.shape().iter().enumerate() {
+        let is_inside = inside(&vertice, object1);
+        if is_inside {
+            collision_info.which_object_which_vertice[1].push(it);
+            collided = true;
         }
     }
-    false
+
+    if collided {
+        return Some(collision_info);
+    }
+    None
 }
 
 fn rotate(vector: &Vec2, theta: f32) -> Vec2 {
@@ -146,16 +204,20 @@ fn inside(vertice: &Vec2, object: &impl Shape) -> bool {
 }
 
 fn reflect(body: &mut Body) {
-    if body.lin_pos.x >= screen_width() {
+    if body.lin_pos.x >= screen_width() * 0.75 {
+        body.lin_pos.x = screen_width() * 0.75;
         body.lin_vel = Vec2::new(-1.0 * body.lin_vel.x, body.lin_vel.y)
     }
-    if body.lin_pos.x <= 0.0 {
+    if body.lin_pos.x <= screen_width() * 0.25 {
+        body.lin_pos.x = screen_width() * 0.25;
         body.lin_vel = Vec2::new(-1.0 * body.lin_vel.x, body.lin_vel.y)
     }
-    if body.lin_pos.y >= screen_height() {
+    if body.lin_pos.y >= screen_height() * 0.75 {
+        body.lin_pos.y = screen_height() * 0.75;
         body.lin_vel = Vec2::new(body.lin_vel.x, -1.0 * body.lin_vel.y)
     }
-    if body.lin_pos.y <= 0.0 {
+    if body.lin_pos.y <= screen_height() * 0.25 {
+        body.lin_pos.y = screen_height() * 0.25;
         body.lin_vel = Vec2::new(body.lin_vel.x, -1.0 * body.lin_vel.y)
     }
 }
